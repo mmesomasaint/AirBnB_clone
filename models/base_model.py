25ed24977dd16ff050a092ba78e3d87f185f9b32
mmesomasaint <mmesomasaint@gmail.com>
2023-03-10 20:20:10 -0800

    Fix: capitalize h in strptime()

--- models/base_model.py
+++ models/base_model.py
@@ -21,7 +21,7 @@ class BaseModel:
         if (kwargs):
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(value, "%Y-%m-%dT%h:%M:%S.%f")
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != '__class__':
                     self.__setattr__(key, value)
         else:
