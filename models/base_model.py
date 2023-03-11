8693e17360aaf7a5962298bf43ffb3f9fcbcb8a1
Bucha1958 <okoriemmadu@gmail.com>
2023-03-11 12:49:53 -0500

    Correction made on the storage file

--- models/base_model.py
+++ models/base_model.py
@@ -28,7 +28,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
-            
+            models.storage.new(self)    
 
     def save(self):
         """
@@ -36,6 +36,8 @@ class BaseModel:
         """
 
         self.updated_at = datetime.today()
+        models.storage.save()
+
 
     def __str__(self):
         """
@@ -43,6 +45,7 @@ class BaseModel:
         """
 
         return ("[BaseModel] ({}) {}".format(self.id, self.__dict__))
+    
     
     def to_dict(self):
         """
